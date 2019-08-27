f75e86f92227c4877ef759c258c90d5cc22d9bfe
pedroaspereira <45178565+pedroaspereira@users.noreply.github.com>
2019-08-27 17:00:33 -0300

    Cancel & Email Notification

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'bdfb2e3fe80ccd',
+    pass: '08d40c0de2ecd6',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
